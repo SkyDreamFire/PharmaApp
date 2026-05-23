4fe2f32b907fa01fbc50e8441fcc4a29c85b23ce
SkyDreamFire <johandjongo5@gmail.com>
2026-05-23 16:22:07 +0100

    colorHearder

--- includes/header.php
+++ includes/header.php
@@ -22,11 +22,11 @@ requireLogin();
 </head>
 <body>
     <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
-            <a class="navbar-brand" href="<?= $_SESSION['user_role'] === 'directeur' ? '/pharma-app/admin/dashboard.php' : '/pharma-app/caissier/dashboard.php' ?>">
+            <a class="navbar-brand" style="padding-right: 50px;" href="<?= $_SESSION['user_role'] === 'directeur' ? '/pharma-app/admin/dashboard.php' : '/pharma-app/caissier/dashboard.php' ?>">
                 <i class="bi bi-capsule-pill me-2"></i>
-                Pharmacie Management
+                FIANGEP Pharma
             </a>
             
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -37,58 +37,58 @@ requireLogin();
                 <ul class="navbar-nav me-auto">
                     <?php if ($_SESSION['user_role'] === 'directeur'): ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/admin/dashboard.php">
+                            <a class="nav-link text-light" href="/pharma-app/admin/dashboard.php">
                                 <i class="bi bi-speedometer2"></i> Dashboard
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/admin/produits.php">
+                            <a class="nav-link text-light" href="/pharma-app/admin/produits.php">
                                 <i class="bi bi-capsule"></i> Médicaments
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/admin/fournisseurs.php">
+                            <a class="nav-link text-light" href="/pharma-app/admin/fournisseurs.php">
                                 <i class="bi bi-truck"></i> Fournisseurs
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/admin/personnel.php">
+                            <a class="nav-link text-light" href="/pharma-app/admin/personnel.php">
                                 <i class="bi bi-people"></i> Personnel
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/admin/ventes.php">
+                            <a class="nav-link text-light " href="/pharma-app/admin/ventes.php">
                                 <i class="bi bi-cart"></i> Ventes
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/admin/stock_alert.php">
+                            <a class="nav-link text-light" href="/pharma-app/admin/stock_alert.php">
                                 <i class="bi bi-exclamation-triangle"></i> Alertes Stock
                             </a>
                         </li>
                     <?php else: ?>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/caissier/dashboard.php">
+                            <a class="nav-link text-light" href="/pharma-app/caissier/dashboard.php">
                                 <i class="bi bi-speedometer2"></i> Dashboard
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/caissier/produits.php">
+                            <a class="nav-link text-light" href="/pharma-app/caissier/produits.php">
                                 <i class="bi bi-capsule"></i> Médicaments
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/caissier/clients.php">
+                            <a class="nav-link text-light" href="/pharma-app/caissier/clients.php">
                                 <i class="bi bi-person"></i> Clients
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/caissier/facture.php">
+                            <a class="nav-link text-light" href="/pharma-app/caissier/facture.php">
                                 <i class="bi bi-receipt"></i> Facturation
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/pharma-app/caissier/stock_mouvement.php">
+                            <a class="nav-link text-light" href="/pharma-app/caissier/stock_mouvement.php">
                                 <i class="bi bi-arrow-up-down"></i> Stock
                             </a>
                         </li>
@@ -97,9 +97,9 @@ requireLogin();
                 
                 <ul class="navbar-nav">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" role="button" data-bs-toggle="dropdown">
+                        <a class="nav-link dropdown-toggle d-flex align-items-center text-light" href="#" role="button" data-bs-toggle="dropdown">
                             <i class="bi bi-person-circle"></i>
-                            <span class="d-none d-md-inline ms-2"><?= escape($_SESSION['user_nom'] . ' ' . $_SESSION['user_prenom']) ?></span>
+                            <span class="d-none d-md-inline ms-2 text-light"><?= escape($_SESSION['user_nom'] . ' ' . $_SESSION['user_prenom']) ?></span>
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end">
                             <li><span class="dropdown-item-text">
